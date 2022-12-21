a4ad6c5e256c8964c5b910219313f86587de2808
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2022-12-21 15:55:29 +0700

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,16 +1,4 @@
-import React, { useState, useEffect } from "react";
-
-import Link from "next/link";
-import { getCategories } from "../services";
-
 const Header = () => {
-  const [categories, setCategories] = useState([]);
-
-  useEffect(() => {
-    getCategories().then((newCategories) => {
-      setCategories(newCategories);
-    });
-  }, []);
 
   return (
     <div className="navbar sticky top-0 z-50" style={{ background: "#f9f2ed" }}>
