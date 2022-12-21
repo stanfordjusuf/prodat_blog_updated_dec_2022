37695a6931ca4bcba4f8f95bea03608833bf284c
stanfordjusuf <107454100+stanfordjusuf@users.noreply.github.com>
2022-12-21 16:01:52 +0700

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Footer = () => (
   <footer className="footer footer-center p-10 bg-primary text-primary-content">
     <img width="50px" height="50px" src="https://i.ibb.co/tbJQrh0/logo.png" alt="logo" border="0" />
-    <p className="font-bold">
-        Prodat <br />
+      <p className="font-bold">
+      Prodat <br />
         Est 2022
-      </p>
+    </p>
     <p>Copyright © 2022 - All right reserved</p>
   </footer>
 );
